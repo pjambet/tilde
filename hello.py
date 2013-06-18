07cf01b8b5a15fb72acbc9ae4b50963a6b8c817a
pjambet <pierre.jambet@gmail.com>
2013-06-18 10:25:11 +0200

    Use templates

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('hello.html')
